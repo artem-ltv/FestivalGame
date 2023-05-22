4af4098c291d5cb1e6763aa6491a2e220dd43294
artem-ltv <60981162+artem-ltv@users.noreply.github.com>
2023-05-22 18:14:19 +0300

    Meteorit magic

--- Assets/Development/Scripts/AimmingMouse.cs
+++ Assets/Development/Scripts/AimmingMouse.cs
@@ -4,11 +4,14 @@ namespace Festival
 {
     public class AimmingMouse : MonoBehaviour
     {
+        public bool IsAimming => _isVisible;
+
         [SerializeField] private GameObject _aim;
         [SerializeField] private LayerMask _layerMask;
 
         private Camera _camera;
         private bool _isVisible;
+        private RaycastHit _hit;
 
         private void Start()
         {
@@ -20,16 +23,22 @@ namespace Festival
             if (_isVisible)
             {
                 Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, _layerMask))
+                if (Physics.Raycast(ray, out _hit, float.MaxValue, _layerMask))
                 {
-                    _aim.transform.position = hit.point;
+                    _aim.transform.position = new Vector3(_hit.point.x, _hit.point.y + 0.1f, _hit.point.z);
                 }
             }
         }
 
-        private void SetVisibility(bool isVisible)
+        public void SetVisibility(bool isVisible)
         {
             _isVisible = isVisible;
+            _aim.SetActive(isVisible);
+        }
+
+        public Vector3 GetAimCoordinates()
+        {
+            return _hit.point;
         }
     }
 }
--- Assets/Development/Scripts/Magic/MeteoriteMagic.cs
+++ Assets/Development/Scripts/Magic/MeteoriteMagic.cs
@@ -1,9 +1,42 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Festival
 {
     public class MeteoriteMagic : MonoBehaviour
     {
-        
+        [SerializeField] private AimmingMouse _aimming;
+
+        private bool _isUsing = false;
+
+        private void Update()
+        {
+            if (_aimming.IsAimming)
+            {
+                if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+                {
+                    SummonMeteorite();
+                    _aimming.SetVisibility(false);
+                }
+            }
+        }
+
+        public void ClickButtonUsingMagic()
+        {
+            _isUsing = !_isUsing;
+            Aim(_isUsing);
+        }
+
+        private void Aim(bool isAimming)
+        {
+            _aimming.SetVisibility(isAimming);
+        }
+
+        private void SummonMeteorite()
+        {
+            Vector3 mousePosition = _aimming.GetAimCoordinates();
+            _isUsing = false;
+            Debug.Log("SummonMeteorit");
+        }
     }
 }
